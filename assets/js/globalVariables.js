07a6bcd0788441eb76d344c18f771330b605e409
xxxDrez <xxxDez@example.com>
2021-10-07 14:24:07 +0300

    erro fix

--- assets/js/globalVariables.js
+++ assets/js/globalVariables.js
@@ -3,7 +3,5 @@
  */
 
  let database = new Database(support.getUserID());
- //let searchStatus = -1;
- //let searchString = '';
  let dialog = null;
  let task = new Tasks(support.taskContainerZeroing(),database.onGetUserId());
